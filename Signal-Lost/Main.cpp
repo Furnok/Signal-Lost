af19bb02387dc58b33cd6eaa8837859811fe51b4
Killian FURNO <killian.furno@orange.fr>
2025-09-27 18:40:18 +0200

    Fix: Check File Safer

--- Signal-Lost/Main.cpp
+++ Signal-Lost/Main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
     file.CreateFileErrors(setupConsole);
 
     // No File
-    if (argv[1] == nullptr)
+    if (argc < 2)
     {
         // Read the File Errors
         file.ReadFileError(setupConsole, "NoFile");
